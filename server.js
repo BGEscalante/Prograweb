8a161441a41340633ec7c7ba4acdb74a329d1153
BGEscalante <b_bryan1363@hotmail.com>
2025-05-21 00:01:34 -0600

    reestructuracion de backend ya finalizado

--- server.js
+++ server.js
@@ -3,19 +3,25 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+// Configuracion Swagger UI
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec)); 
+
 // Importar rutas
-const rutas = require('./Rutas/rutas');
+const rutas = require('./Backend/Rutas/rutas');
 
 // Configurar rutas
 app.use('/api', rutas);
 
 // Conexión a BD
-const db = require('./Conexion/db');
+const db = require('./Backend/Conexion/db');
 db.query('SELECT 1')
   .then(() => console.log('Conexión a BD exitosa wujuuu'))
   .catch((error) => {
